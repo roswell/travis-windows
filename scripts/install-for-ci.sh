e702890a1bb08e8615790fbacc6f60543f48a641
SANO Masatoshi <snmsts@gmail.com>
2019-06-02 02:49:28 +0800

    unzip -> copy

--- scripts/install-for-ci.sh
+++ scripts/install-for-ci.sh
@@ -112,7 +112,9 @@ fi
 if uname -s | grep -e MSYS_NT;then
     curl -L "https://ci.appveyor.com/api/projects/snmsts/roswell-en89n/artifacts/Roswell-x86_64.zip?branch=master&job=Environment%3A%20MSYS2_ARCH%3Dx86_64,%20MSYS2_BITS%3D64,%20MSYSTEM%3DMINGW64,%20METHOD%3Dcross" --output /tmp/roswell.zip
     ls -l /tmp
-    unzip /tmp/roswell.zip -d ~/.roswell/bin/
+    unzip /tmp/roswell.zip -d /tmp/
+    cp /tmp/roswell/ros.exe ~/.roswell/bin
+    cp -r /tmp/roswell/lisp ~/.roswell/bin/lisp
 fi
 
 if ! which ros >/dev/null; then
